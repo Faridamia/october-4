195476dda7a580bfc592a6b8772f8f78ba529391
Your Name <youremail@yourdomain.com>
2023-10-19 13:45:58 +0600

    commit

--- bin/october_4.dart
+++ bin/october_4.dart
@@ -1,5 +1,31 @@
-import 'package:october_4/october_4.dart' as october_4;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${october_4.calculate()}!');
+void main() {
+  passwordGenerator();
+}
+
+void passwordGenerator() {
+  String answer = '';
+  do {
+    String str = '1234567890rtuhgfvbn';
+    String password = '';
+    print('Из скольки символов должен состоять пароль? \n4\n6\n8\n10\n12');
+    int passLength = int.tryParse(stdin.readLineSync() ?? '') ?? 4;
+    if (passLength != 4 &&
+        passLength != 6 &&
+        passLength != 8 &&
+        passLength != 10 &&
+        passLength != 12) {
+      print('Не правильная длина пароля');
+    } else {
+      for (int i = 1; i <= passLength; i++) {
+        int randomIndex = Random().nextInt(str.length);
+        password += str[randomIndex];
+      }
+      print(password);
+      print('Подобрать другой пароль? Y/N');
+      answer = stdin.readLineSync() ?? '';
+    }
+  } while (answer.toUpperCase() != 'N');
 }
